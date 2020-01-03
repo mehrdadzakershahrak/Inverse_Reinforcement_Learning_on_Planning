afccadfe8345612ddde682e6b78c06da7c9f2504
root <zaker.mehrdad@gmail.com>
2020-01-03 22:22:43 +0000

    updated run_vi

--- run_vi.py
+++ run_vi.py
@@ -1,12 +1,12 @@
 import numpy as np
 from value_iteration import value_iteration
 import pickle
-thetas = np.array([-142.12235333 ,-78.4391271 ,-142.26565079])
+#thetas = np.array([-142.12235333 ,-78.4391271 ,-142.26565079])
 feat_map = np.load("feat_map_final.npy")
-rewards = np.dot(feat_map,thetas)
+rewards = np.load("rewards.npy")
 P_a = np.load("P_a.npy")
 
-with open('states_dict.pickle', 'r') as file:
+with open('states_dict.pickle', 'rb') as file:
         states_dict=pickle.load(file)
     
 gamma = 0.9
@@ -14,22 +14,23 @@ values,policy= value_iteration(P_a, rewards, gamma, error=0.01, deterministic=Tr
 print(policy)
 print(values)
 print(rewards)
-init_state = (1)
+init_state = (3,4)
+state = init_state
 print(states_dict)
 state_id = states_dict[init_state] #no HAS_ELECTRICITY
-with open('all_actions.pickle', 'r') as file:
+with open('all_actions.pickle', 'rb') as file:
         all_actions=pickle.load(file)
 
 reverse_actions={}
 
-for key,value in all_actions:
-	reverse_actions[value]=key
+for key in all_actions.keys():
+	reverse_actions[all_actions[key]]=key
 
 while True:
-	action = policy[state_id]
+	action = int(policy[state_id])
 	action_template = reverse_actions[action]
 	print(action_template)
-	state = list(state).append(action)
-	state_id=states_dict[state]
-
+	state = list(state)
+	state.append(action)
+	state_id=states_dict[tuple(sorted(state))]
 
